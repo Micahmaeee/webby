edce76c98e4ba02408e9d034b9670f62d131ea26
Micahmaeee <66246634+Micahmaeee@users.noreply.github.com>
2020-10-11 18:12:16 +0800

    Add files via upload

---
+++ micahdestor.php
@@ -0,0 +1,122 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie-edge" />
+    <title>Shapes</title>
+    <link rel="stylesheet" href="./micahdestorcss.css" />
+</head>
+<body>
+    <svg width="1980" height="1080">
+        <rect x="50" y="20" width="200" height="300" fill="pink" stroke="white" stroke-width= "2"/>         
+        <circle r="70" cx="150" cy="150" fill="lavender"/>
+        <circle r="35" cx="199" cy="75" fill="lavender"/>
+        <circle r="35" cx="99" cy="75" fill="lavender"/>
+        <circle r="25" cx="190" cy="135" fill="white"/>
+        <circle r="25" cx="110" cy="135" fill="white"/>
+        <circle r="15" cx="190" cy="135" fill="black"/>
+        <circle r="15" cx="110" cy="135" fill="black"/>
+        <circle r="15" cx="190" cy="125" fill="white"/>
+        <circle r="15" cx="110" cy="125" fill="white"/>
+        <line x1="100" y1="175" x2="60" y2="170" style="stroke:rgb(0,0,0);stroke-width:2" />
+        <line x1="100" y1="180" x2="60" y2="180" style="stroke:rgb(0,0,0);stroke-width:2" />
+        <line x1="100" y1="185" x2="60" y2="190" style="stroke:rgb(0,0,0);stroke-width:2" />
+        <line x1="200" y1="175" x2="240" y2="170" style="stroke:rgb(0,0,0);stroke-width:2" />
+        <line x1="200" y1="180" x2="240" y2="180" style="stroke:rgb(0,0,0);stroke-width:2" />
+        <line x1="200" y1="185" x2="240" y2="190" style="stroke:rgb(0,0,0);stroke-width:2" />
+        <circle r="10" cx="150" cy="175" fill="black"/>
+        <line x1="150" y1="185" x2="140" y2="200" style="stroke:rgb(0,0,0);stroke-width:2" />
+        <line x1="150" y1="185" x2="160" y2="200" style="stroke:rgb(0,0,0);stroke-width:2" />
+        <text x="220" y="20">
+            <tspan x="90" dy="330">1. Circles and Line</tspan>
+        </text>
+        <rect x="300" y="20" width="200" height="300" fill="lightpink" stroke="white" stroke-width= "2"/>
+        <rect x="300" y="20" width="200" height="100" fill="lightpink" stroke="white" stroke-width= "2"/>
+        <circle r="49" cx="350" cy="70" fill="lavender"/>
+        <circle r="40.5" cx="439" cy="61" fill="lavender"/>
+        <circle r="18" cx="481" cy="101" fill="lavender"/>
+        <rect x="300" y="120" width="200" height="100" fill="lavender" stroke="white" stroke-width= "2"/>
+        <circle r="49" cx="450" cy="170" fill="lightpink"/>
+        <circle r="40.5" cx="362" cy="161" fill="lightpink"/>
+        <circle r="18" cx="320" cy="201" fill="lightpink"/>
+        <rect x="300" y="220" width="200" height="100" fill="lightpink" stroke="white" stroke-width= "2"/>
+        <circle r="49" cx="350" cy="270" fill="lavender"/>
+        <circle r="40.5" cx="439" cy="261" fill="lavender"/>
+        <circle r="18" cx="481" cy="301" fill="lavender"/>
+        <text x="220" y="20">
+            <tspan x="325" dy="330">
+            <tspan x="325" dy="330">2. Circle and Rectangle</tspan>
+        </text>
+        <rect x="550" y="20" width="200" height="300" fill="lightpink" stroke="white" stroke-width= "2"/>
+        <polyline points="552 ,35 600, 35 600, 85 650, 85 650, 135 700, 135 700, 185 750, 185, 750," fill="none" stroke="white"stroke-width= "10" />
+        <polyline points="552 ,85 600, 85 600, 135 650, 135 650, 185 700, 185 700, 235 750, 235, 750," fill="none" stroke="white"stroke-width= "10" />
+        <polyline points="552 ,135 600, 135 600, 185 650, 185 650, 235 700, 235 700, 285 750, 285, 750," fill="none" stroke="white"stroke-width= "10" />
+        <polyline points="552 ,185 600, 185 600, 235 650, 235 650, 285 700, 285 700, 320 730" fill="none" stroke="white"stroke-width= "10" />
+        <circle r="40" cx="700" cy="75" fill="white"/>
+        <circle r="30" cx="590" cy="278" fill="white"/>
+
+        <text x="220" y="20">
+        <tspan x="575" dy="330">3. Polyline and Circle</tspan>
+    </text>
+    <rect x="800" y="20" width="200" height="300" fill="lightpink" stroke="white" stroke-width= "2"/> 
+    <ellipse cx="900" cy="170" rx="97" ry="150" fill="lavender" />
+    <polygon points="900,50 840,240 980,120 820,120 960,240" fill="white" stroke="" stroke-width= "5"/>
+    <text x="220" y="20">
+        <tspan x="830" dy="330">4. Polygon and Ellipse</tspan>
+    </text>
+    <rect x="150" y="400" width="200" height="300" fill="white" stroke="white" stroke-width= "5"/>
+    <rect x="150" y="402" width="200" height="100" fill="lightpink" stroke="white" stroke-width= "2"/>
+    <rect x="150" y="502" width="200" height="100" fill="lavender" stroke="white" stroke-width= "2"/>
+    <line x1="250" y1="410" x2="250" y2="495" style="stroke:white;stroke-width:3" />
+    <line x1="250" y1="510" x2="250" y2="595" style="stroke:lightpink;stroke-width:3" />
+    <line x1="250" y1="610" x2="250" y2="695" style="stroke:lavender;stroke-width:3" />
+    <circle r="20" cx="200" cy="450" fill="lavender"/>
+    <circle r="20" cx="280" cy="430" fill="lavender"/>
+    <circle r="20" cx="320" cy="475" fill="lavender"/>
+    <circle r="20" cx="200" cy="550" fill="white"/>
+    <circle r="20" cx="300" cy="550" fill="white"/>
+    <circle r="15" cx="170" cy="625" fill="lightpink"/>
+    <circle r="15" cx="170" cy="680" fill="lightpink"/>
+    <circle r="15" cx="225" cy="625" fill="lightpink"/>
+    <circle r="15" cx="225" cy="680" fill="lightpink"/>
+
+    <text x="220" y="20">
+        <tspan x="160" dy="715">5. Circle, Line and Rectangle</tspan>
+    </text>
+    <rect x="400" y="400" width="200" height="300" fill="lightpink" stroke="white" stroke-width= "2"/>
+    <polyline points="400 ,411 450, 411 450,460 500, 460, 500, 510 550, 510 550, 560 600,560 600" fill="none" stroke="white"stroke-width= "15" />
+    <polyline points="400 ,431 430, 431 430,480 480, 480 480, 530 530, 530 530, 580 600, 580 580" fill="none" stroke="lavender"stroke-width= "15" />
+    <polyline points="400 ,451 410, 451 410,500 460, 500 460, 550 510, 550 510, 600 600, 600 600" fill="none" stroke="white"stroke-width= "15" />
+    <polyline points="400 , 520 440, 520 440, 570 490, 570 490, 620 600, 620 620" fill="none" stroke="lavender"stroke-width= "15" />
+    <polyline points="400 , 540 420, 540 420, 590 470, 590 470, 640 600, 640 640" fill="none" stroke="white"stroke-width= "15" />
+    <polygon points="520 680, 580 680, 600 630, 550 600, 500 630, 520 680 " fill="lavender" stroke="white" stroke-width= "2"/>
+<text x="220" y="20">
+        <tspan x="420" dy="715">6. polyline and polygon</tspan>
+    </text>
+    <rect x="650" y="400" width="200" height="300" fill="lightpink" stroke="white" stroke-width= "2"/>
+    <polygon points="675 700, 825 700, 850 675, 825 650, 675 650, 650 675" fill="lavender" stroke="" stroke-width= "2"/>
+    <polygon points="675 450, 825 450, 850 425, 825 400, 675 400, 650 425" fill="lavender" stroke="" stroke-width= "2"/>
+    <polyline points=" 690 ,451 690, 451 690,485 740, 485 740, 535 790, 535 790,585 840, 585 840, 650 865 " fill="none" stroke="white"stroke-width= "15" />
+    <polyline points=" 675 ,451 675, 451 675,500 725, 500 725, 550 775, 550 775,600 825, 600 825, 650 850 " fill="none" stroke="lavender"stroke-width= "15" />
+    <polyline points=" 660 ,451 660, 451 660,515 710, 515 710, 565 760, 565 760,615 810, 615 810, 650 835 " fill="none" stroke="white"stroke-width= "15" />
+    <rect x="748" y="479" width="100" height="51" fill="lavender" stroke="white" stroke-width= "2"/>
+    <circle r="20" cx="824" cy="555" fill="white"/>
+    <ellipse cx="700" cy="600" rx="52" ry="27" fill="lavender" />
+    <circle r="20" cx="675" cy="555" fill="white"/>
+    <line x1="800" y1="700" x2="800" y2="400" style="stroke:white;stroke-width:3" />
+    <line x1="700" y1="700" x2="700" y2="400" style="stroke:white;stroke-width:3" />
+    <line x1="750" y1="700" x2="750" y2="400" style="stroke:white;stroke-width:3" />
+
+    <text x="220" y="20">
+        <tspan x="650" dy="715">7. line, circle, rectangle, polyline, polygon, and ellipse</tspan>
+    </text>
+
+
+
+
+    
+    </svg>
+</body>
+</html>
